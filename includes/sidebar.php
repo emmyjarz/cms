79f38efa0599a6a0041769efebabb2341e6a2141
Emmy Jarzebinski <emmyjarz@gmail.com>
2018-02-03 20:43:21 -0800

    sidebar categories

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,4 +1,3 @@
-   
    <div class="col-md-4">
                 <!-- Blog Search Well -->
                 <div class="well">
@@ -15,6 +14,14 @@
                     </form><!--search form-->
                     <!-- /.input-group -->
                 </div>
+<?php
+$query = "SELECT * FROM categories LIMIT 3";
+$select_categories_sidebar = mysqli_query($connection, $query);
+
+
+
+
+?>
 
                 <!-- Blog Categories Well -->
                 <div class="well">
@@ -22,14 +29,12 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+<?php
+while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+    $cat_title = $row['cat_title'];
+echo "<li><a href='#'>{$cat_title}</a></li>";
+}
+?>
                             </ul>
                         </div>
                         <!-- /.col-lg-6 -->
@@ -51,9 +56,7 @@
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "widget.php" ?>
+               
 
             </div>

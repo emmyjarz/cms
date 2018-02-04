4c61753346112ec91c57159be73696349a550eac
Emmy Jarzebinski <emmyjarz@gmail.com>
2018-02-03 20:07:20 -0800

    search post

--- search.php
+++ search.php
@@ -17,16 +17,25 @@
                     <small>Secondary Text</small>
                 </h1>
                 <?php
-$query = "SELECT * FROM posts";
-$select_all_posts_query = mysqli_query($connection, $query);
-while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-    $post_title = $row['post_title'];
-    $post_author = $row['post_author'];
-    $post_date = $row['post_date'];
-    $post_image = $row['post_image'];
-    $post_content = $row['post_content'];
-?> 
-    
+if (isset($_POST['submit'])) {
+    $search = $_POST['search'];
+    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
+    $search_query = mysqli_query($connection, $query);
+    if (!$search_query) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+    $count = mysqli_num_rows($search_query);
+    if ($count == 0) {
+        echo "<h1> NO RESULT </h1>";
+    } else {
+        while ($row = mysqli_fetch_assoc($search_query)) {
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            $post_date = $row['post_date'];
+            $post_image = $row['post_image'];
+            $post_content = $row['post_content'];
+            ?>
+
 
                 <!-- First Blog Post -->
                 <h2>
@@ -43,20 +52,15 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
-
-
-<?php
+                <?php
 }
-
+    }
+}
 ?>
-
-           
-            </div>
+          </div>
 
             <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.php"?>
-
 
         </div>
         <!-- /.row -->

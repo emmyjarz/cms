7b0bc47f0a587abe2a8a852a55212bea08b0f337
Emmy Jarzebinski <emmyjarz@gmail.com>
2018-02-06 22:59:17 -0800

    admin create post form

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -28,14 +28,14 @@
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
-                    <li>
+                    <li class="active">
                         <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="./posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
@@ -45,7 +45,7 @@
                     <li>
                         <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
-                    <li class="active">
+                    <li>
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
                     </li>                 
                     <li>
---
+++ admin/posts.php
@@ -0,0 +1,58 @@
+<?php include "includes/admin_header.php"?>
+<?php include "function.php"?>
+<!-- already have ob_start in header is enough -->
+<?php ob_start();?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                   <h1 class="page-header">
+                        Welcome to admin
+                        <small>Author</small>
+                    </h1>        
+                 
+                           <?php
+// if(isset($_GET['source'])){
+//     $source = $_GET['source'];
+// } else{
+//     $source = "";
+// }
+switch ($_GET['source']){
+    case "add_post";
+    include "includes/add_post.php";
+
+    break;
+
+    case  "50";
+    echo "NICE 50";
+    break;
+
+    default:
+    include "includes/view_all_post.php";
+    break;
+
+}
+?>  
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include "includes/admin_footer.php"?>

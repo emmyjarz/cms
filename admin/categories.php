773728cc31adbe949b03cd8024cd3816f9ea29fc
Emmy Jarzebinski <emmyjarz@gmail.com>
2018-02-06 21:12:13 -0800

    refactoring category

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,8 @@
 <?php include "includes/admin_header.php"?>
+<?php include "function.php"?>
+<!-- already have ob_start in header is enough -->
+<?php ob_start(); ?>
+
 <div id="wrapper">
 
     <!-- Navigation -->
@@ -16,21 +20,8 @@
                         <small>Author</small>
                     </h1>
                     <div class="col-xs-6">
-                        <?php //ADD CATEGORY
-if (isset($_POST['submit'])) {
-    $cat_title = $_POST['cat_title'];
-    if ($cat_title == "" || empty($cat_title)) {
-        echo "This field cannot be empty";
-    } else {
-        $query = "INSERT INTO categories (cat_title) ";
-        $query .= "VALUES ('{$cat_title}')";
-        $create_category_query = mysqli_query($connection, $query);
-        if (!$create_category_query) {
-            die("FAIL" . mysqli_error($connection));
-        }
-    }
-}
-?>
+                        <!-- ADD CATEGORY -->
+                        <?php insert_categories()?>
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat-title">Add Category</label>
@@ -45,12 +36,10 @@ if (isset($_POST['submit'])) {
 if (isset($_GET['edit'])) {
     include "includes/update_categories.php";
 }
-
 ?>
                      </div>
                     <!-- Add Category Form -->
                     <div class="col-xs-6">
-
                         <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
@@ -59,31 +48,8 @@ if (isset($_GET['edit'])) {
                                 </tr>
                             </thead>
                             <tbody>
-
-                                <?php //FIND ALL CATEGORIES QUERY
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection, $query);
-while ($row = mysqli_fetch_assoc($select_categories)) {
-    $cat_id = $row['cat_id'];
-    $cat_title = $row['cat_title'];
-    echo "<tr>";
-    echo "<td>{$cat_id}</td>";
-    echo "<td>{$cat_title}</td>";
-    echo "<td><a href='categories.php?delete={$cat_id}'>DELETE</a></td>";
-    echo "<td><a href='categories.php?edit={$cat_id}'>EDIT</a></td>";
-    echo "</tr>";
-}
-
-?>
-                                <?php //DELETE QUERY
-if (isset($_GET['delete'])) {
-    $the_cat_id = $_GET['delete'];
-    $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-    $delete_query = mysqli_query($connection, $query);
-    //refresh page
-    header("Location: categories.php");
-}
-?>
+                                <?php findAllCategories()?>
+                                <?php deleteCategories()?>
                             </tbody>
                         </table>
                     </div>
